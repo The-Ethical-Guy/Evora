7a9ad3aa063159ab57cb643f9964ba4b46676107
TheEthicalGuy <138525574+The-Ethical-Guy@users.noreply.github.com>
2024-07-10 22:56:17 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ echo -e "\033[0m            \033[1;97m\033[1;31;40m█        █████  �
 echo -e "\033[0m            \033[1;97m\033[1;31;40m██████████████████████████████████████████████████\033[0m  \033[1;93;40mBeta\033[0m"
 echo -e ""
 echo -e "\033[1;33m FOR DONATIONS"
-echo -e "\033[1;32m BTC: bc1qeqn0lcykrpcvkgcyse4vphrqmffu26sz9dnuru"
+echo -e "\033[1;32m BTC: bc1qhpez52mku3d532xsrz7f0zn5l85e2q47zsr6l2"
 echo -e "\033[1;32m LTC: ltc1q4u7prf66nk9d5u3s0ha30ffcfws7vhq7dad946"
 
 tool_name="evora"

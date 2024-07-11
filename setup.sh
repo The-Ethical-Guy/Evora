853b430949762a0f2dd42533a0027c0ecdd2ee2a
TheEthicalGuy <138525574+The-Ethical-Guy@users.noreply.github.com>
2024-07-11 00:36:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,8 +10,8 @@ echo -e "\033[0m            \033[1;97m\033[1;31;40m█        █████  �
 echo -e "\033[0m            \033[1;97m\033[1;31;40m██████████████████████████████████████████████████\033[0m  \033[1;93;40mBeta\033[0m"
 echo -e ""
 echo -e "\033[1;33m FOR DONATIONS"
-echo -e "\033[1;32m BTC: bc1qhpez52mku3d532xsrz7f0zn5l85e2q47zsr6l2"
-echo -e "\033[1;32m LTC: ltc1qls08k4s7gzuu2dx83znpng20vw86uc2h8xnwsc"
+echo -e "\033[1;32m BTC: bc1qeqn0lcykrpcvkgcyse4vphrqmffu26sz9dnuru"
+echo -e "\033[1;32m LTC: ltc1q4u7prf66nk9d5u3s0ha30ffcfws7vhq7dad946"
 
 tool_name="evora"
 script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"  
@@ -23,7 +23,10 @@ sudo mv $tool_name /usr/local/bin/
 if [ -f "$script_path/requirements.txt" ]; then
     echo ""
     echo -e "\033[1;32m Installing requirements..."
-
+    
+    pip3 install --upgrade pip > pip_install_output.txt 2>&1
+    apt-get update > pip_install_output.txt 2>&1
+    apt-get install python3-dev build-essential > pip_install_output.txt 2>&1
     pip3 install -r "$script_path/requirements.txt" > pip_install_output.txt 2>&1
     rm pip_install_output.txt
     sleep 5

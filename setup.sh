6481b3264d523fd2478e3d388474474e7ab35b53
TheEthicalGuy <138525574+The-Ethical-Guy@users.noreply.github.com>
2024-07-12 02:26:03 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,8 +26,8 @@ venv_path="$script_path/venvEvora"
 if ! dpkg -s python3-venv &> /dev/null; then
     echo -e "\033[1;33mInstalling python3-venv package...\033[0m"
     sudo apt install python3-venv -y > pip_install_output.txt 2>&1
-    sudo apt install python3-pip -y
-    sudo apt install python3-virtualenv
+    sudo apt install python3-pip -y > pip_install_output.txt 2>&1
+    sudo apt install python3-virtualenv -y > pip_install_output.txt 2>&1
     if [ $? -ne 0 ]; then
         echo -e "\033[1;31mFailed to install python3-venv. Check pip_install_output.txt for details.\033[0m"
         exit 1
